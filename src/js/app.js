53708dea64f592f9f3c287bc71c82da66633e670
Michaela Blackham <Michaelablackham@gmail.com>
2017-05-23 06:47:19 -0600

    started adding some basic JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,73 @@
 // (function() {
   'use strict';
-
+  //////////////////////////////////////////
   //object state
-  var state = [];
+  //////////////////////////////////////////
+  var state = {
+    // questions: [
+    //   {
+    //     rgba: rgbValue
+    //   }
+    // ]
+  };
 
-  var colors = [];
+  var colors = {
+    value: []
+  };
 
+  //////////////////////////////////////////
   //Variables
+  //////////////////////////////////////////
   var min = 0;
   var max = 250;
 
+  var progressItem = [
+    '<li class="@result">',
+      '<i class="fa fa-@check" aria-hidden="true"></i>',
+    '</li>'
+  ].join('');
+
+  //////////////////////////////////////////
   //state modification functions
+  //////////////////////////////////////////
+
+  //CHOOSE ITEM ITEM
+  // function checkItem (state, index, newState) {
+  //   state.items[index] = newState;
+  // }
+
+  $(".options--input input").click(function () {
+    $(".options--input").removeClass('active');
+    $(this).parent().addClass('active');
+  });
+
+  //////////////////////////////////////////
+  //functions that render state
+  //////////////////////////////////////////
   //create a for loop to gather 3 random numbers
-  function getRandomNumber(min, max) {
-    console.log( Math.floor(Math.random()) );
-
+  var RGBValue = function (min, max) {
     for (var i = 0; i <3; i++) {
-
+      connsole.log (Math.floor(Math.random() * 250 - 1)) ;
     }
   }
 
-  getRandomNumber();
-
-  //functions that render state
-  $('button.start').click(function () {
-    $('header').addClass('active');
-    $('body').removeClass('home');
-    $('#home').fadeOut();
-    $('#question').fadeIn();
-  });
-
+  //////////////////////////////////////////
   //event listeners
+  //////////////////////////////////////////
 
   function startButton () {
-
+    $('button.start').click(function () {
+      $('header').addClass('active');
+      $('body').removeClass('home');
+      $('#home').hide();
+      $('#question').fadeIn('slow');
+      $('.pager').fadeIn();
+    });
   }
 
   function initiateQuiz () {
-
+    startButton ();
   }
-
+  //NEW RGB VALUE
   initiateQuiz();
 // });

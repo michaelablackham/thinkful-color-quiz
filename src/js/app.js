768430210baa8885004471471dd05a857748960e
Michaela Blackham <Michaelablackham@gmail.com>
2017-06-09 10:52:16 -0600

    addditions to incorrect and correct answer praises

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,25 @@ $(function() {
     question: [],
     correctEmoji: '(งツ)ว',
     correctText: [
-      'Great job! You must live in a  world of rainbows and unicorns!',
+      'Great job! You must live in a world of rainbows and unicorns!',
       'Did you really know the answer? Or did you just pick your favorite color?',
       'Yassss queeeen. You got it',
-      "Taste the rainbow bitches"
+      'You. Is. Good.',
+      'I see "Professional Color Identifier" in your future!',
+      'Well, color me impressed!',
+      'You got lucky that time...',
+      'We\'re going to get along just OK'
     ],
     wrongEmoji: '(╯°□°）╯︵ ┻━┻',
     wrongText: [
       'Seriously? Is your favorite color black, like your soul?',
-      'Uuuuugggghhhhhhhhh',
-      'The world is a sad place because of your color choice',
-      '50 shades of GRAY must be your favorite book....'
+      'Uuuuugggghhhhhhhhh.',
+      'The world is a sad place because of your color choice.',
+      '50 shades of GRAY must be your favorite book....',
+      'You do know what a color is, right?',
+      'Preschool must have been tough for you.',
+      'On Wednesdays, you WISH you wore pink.',
+      'Something tells me you\'d be better suited for identifying smells.'
     ],
     score: 0,
     currentQuestion: 0,
@@ -116,11 +124,11 @@ $(function() {
   }
 
   function randomPraise () {
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * 8);
   };
 
   function randomWrong () {
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * 8);
   };
 
   //////////////////////////////////////////
